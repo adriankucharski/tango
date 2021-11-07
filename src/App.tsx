44267a12882a42d45474ca51fb3217d4c3d9a777
Adrian <32023460+adriankucharski@users.noreply.github.com>
2021-11-07 18:44:06 +0100

    Test api response

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+const App = () => {
+  const [apiData, setApiData] = useState<string>('Czekam na serwer');
+
+  useEffect(() => {
+    fetch('')
+      .then(response => {
+        if (response.ok)
+          return response.json();
+      })
+      .then(data => setApiData(data));
+  }, [apiData]);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>{apiData}</p>
       </header>
     </div>
   );
