bb28487cbedec1e9415bac60a1611d8df0cf760a
Adrian <32023460+adriankucharski@users.noreply.github.com>
2021-12-19 22:14:13 +0100

    Check invalid board url

--- src/containers/WorkBoard/index.tsx
+++ src/containers/WorkBoard/index.tsx
@@ -1,6 +1,21 @@
+import axios from 'axios';
+import { useEffect } from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
 import Board from '../../components/Board';
+import { API_URL } from '../../hooks/useGlobalContext';
+import { BoardType } from '../BoardList';
 
 const WorkBoard = () => {
+  const { boardID, boardName } = useParams();
+  const navigate = useNavigate();
+  useEffect(() => {
+    axios.get(`${API_URL}/board/all`)
+      .then(r => {
+        const data: BoardType[] = r.data;
+        if (!data.some(el => `${el.id}` === boardID && el.name === boardName))
+          navigate('/tango');
+      });
+  }, []);
 
   return (
     <main className="bg-trello h-[100vh] w-[fit-content] min-w-full">
